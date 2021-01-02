472eab26604ef5c5307b7cde5b3fcebfa779139a
lim01 <lim01@naver.com>
2021-01-02 18:12:20 +0900

    Blog Configuration

---
+++ src/main/java/com/dingsam/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.dingsam.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
